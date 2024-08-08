c73d92737885c5ee770a0c03af3200990e786977
“Reese <reesegreen2014@me.com>
2024-08-08 12:58:20 -0600

    expands ArticleList with mocked data and consistent image styling

--- src/components/ArticleList/ArticleList.js
+++ src/components/ArticleList/ArticleList.js
@@ -1,25 +1,54 @@
-import React from 'react';
-import placeholderArticles from '../../mockData';
-import './ArticleList.css'
+import React, { useEffect, useState } from 'react';
+import { fetchArticles } from '../../ApiCalls/apiCalls';
+import './ArticleList.css';
+import placeholderImage from '../../images/newsImage.jpg';
 
 const ArticleList = () => {
-    return (
-      <div className="article-list">
-        {placeholderArticles.map((article, index) => (
-          <div key={index} className="article-card">
-            {article.urlToImage && (
-              <img src={article.urlToImage} alt={article.title} className="article-image" />
-            )}
-            <div className="article-details">
-              <h2 className="article-title">{article.title}</h2>
-              <p className="article-description">{article.description}</p>
-              <p className="article-date">{new Date(article.publishedAt).toLocaleDateString()}</p>
-              <p className="article-source">{article.source.name}</p>
-            </div>
+  const [articles, setArticles] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    setLoading(true);
+    fetchArticles()
+      .then(articlesData => {
+        console.log('Fetched articles:', articlesData);
+        setArticles(articlesData);
+        setLoading(false);
+      })
+      .catch(error => {
+        setError(error.message);
+        setLoading(false);
+      });
+  }, []);
+
+  if (loading) {
+    return <p>Loading articles...</p>;
+  }
+
+  if (error) {
+    return <p>Error loading articles: {error}</p>;
+  }
+
+  return (
+    <div className="article-list">
+      {articles.map((article, index) => (
+        <div key={index} className="article-card">
+          <img
+            src={article.urlToImage || placeholderImage} 
+            alt={article.title}
+            className="article-image"
+          />
+          <div className="article-details">
+            <h2 className="article-title">{article.title}</h2>
+            <p className="article-description">{article.description}</p>
+            <p className="article-date">{new Date(article.publishedAt).toLocaleDateString()}</p>
+            <p className="article-source">{article.source.name}</p>
           </div>
-        ))}
-      </div>
-    );
-  };
-  
+        </div>
+      ))}
+    </div>
+  );
+};
+
 export default ArticleList;
