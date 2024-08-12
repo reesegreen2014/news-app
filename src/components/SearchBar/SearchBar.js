3dde78759ecee4602890eaf7297f28f0095f3443
“Reese <reesegreen2014@me.com>
2024-08-12 10:08:18 -0600

    adds state management and input handling

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,11 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 
-const SearchBar = () => {
+const SearchBar = ({onSearch}) => {
+    const [search, setSearch] = useState('');
+
+    const handleInputChange = (event) => {
+        setSearch(event.target.value);
+    }
+
+    const handleSearch = (event) => {
+        event.preventDefault();
+        onSearch(search);
+    }
     return (
         <form className="search-bar">
             <input 
                 type="text"
                 placeholder="search articles..."
+                value={search}
+                onChange={handleInputChange}
                 className="search-input"
             />
             <button type="submit" className="search-button">Search</button>
